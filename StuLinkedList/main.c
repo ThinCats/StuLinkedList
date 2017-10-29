09ba450980d62dd9ec7427f6f9b5dc023ad46b2f
Brody <Thincats@163.com>
2017-10-29 14:06:26 +0800

    Add a new UI

--- StuLinkedList/main.c
+++ StuLinkedList/main.c
@@ -45,9 +45,84 @@ node * reIdList(node * head, int startId);
 //the head is not changed
 int calTheAve(node * head);
 
+//Order the list
+node * orderAList(node * head);
 
+
+void printSuc(void);
+void printWelcome(void);
+void printStars(int n);
 int main()
 {
-    printf("Hello world!\n");
+    node *stuList = (node *)malloc(sizeof(node));
+
+    //UI:
+    printWelcome();
+
+    int n;
+    scanf("%d", &n);
+
+    switch(n)
+    {
+    case 1:
+        {
+            creatAList(stuList);
+            break;
+        }
+    case 2:
+        {
+            if(calTheAve(stuList) == 1)
+                printSuc();
+            break;
+        }
+    case 3:
+        {
+            printf("You want to add after which one?(I mean the nodeId\n>>)");
+            int nodeId;
+            scanf("%d", &nodeId);
+
+            if((stuList = insertANode(stuList, nodeId)));
+                printSuc();
+        }
+    case 4:
+        {
+            printf("You want to delete which one?(I mean the nodeId\n>>)");
+            int nodeId;
+            scanf("%d", &nodeId);
+
+            if(stuList = deleteAnode(stuList, nodeId))
+                printSuc();
+        }
+    case 5:
+        {
+            printAList(stuList);
+        }
+    case 6:
+        {
+            if(stuList = orderAList(stuList))
+                printSuc();
+        }
+    }
+
     return 0;
+}
+
+void printSuc(void)
+{
+    printf("It's successful. Print it to see\n");
+}
+
+void printWelcome(void)
+{
+    int n = 15;
+    printStars(n);
+    //purchar('\n');
+    printf("\t\tPlease choose what you want:\n")
+    printf("\t\t1. insert\n");
+    printf("\t\t2. Calculate the Average Score\n");
+    printf("\t\t3. Add a new student\n");
+    printf("\t\t4. Delete an old student\n");
+    printf("\t\t5. Print The table\n");
+    printf("\t\t6. Order them by the Ave score\n");
+    printStars(n);
 }
