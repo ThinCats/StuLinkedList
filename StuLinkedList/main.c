81bf63ee800f203793e2bd0da3c8289cad922fd3
Brody <Thincats@163.com>
2017-10-29 14:19:26 +0800

    First to use the header(Multi-source-file)

--- StuLinkedList/main.c
+++ StuLinkedList/main.c
@@ -1,52 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-struct StuInfo
-{
-    char id[20];
-    char name[20];
-    char sex;    //w = female; m = male;
-};
+#include "stuListModel.h"
 
-//Student's score
-struct StuScore
-{
-    int chinese;
-    int math;
-    int English;
-    float average;
-};
-
-//LinkedList
-struct node
-{
-    struct StuInfo info;
-    struct StuScore score;
-    int nodeId;     //To tag each node;
-    struct node * next;    //Point to the next;
-};
-typedef struct node node;
-
-//Create and record the data;
-node * creatAList(node * head);
-
-node * deleteAnode(node * head, int nodeId);
-
-void printAList(node * head);
-
-//insert a node inside some ids(中文来讲就是在某序号下一个插入节点)
-node * insertANode(node * head, int nodeId_before);
-
-//So We need to reId all the nodes
-//startId means whether you want to start with 1 or 0;
-node * reIdList(node * head, int startId);
-
-//calculate the average score
-//return whether is successful
-//the head is not changed
-int calTheAve(node * head);
-
-//Order the list
-node * orderAList(node * head);
 
 
 void printSuc(void);
@@ -58,6 +13,7 @@ int main()
 
     //UI:
     printWelcome();
+    putchar('\n');
 
     int n;
     scanf("%d", &n);
@@ -117,7 +73,7 @@ void printWelcome(void)
     int n = 15;
     printStars(n);
     //purchar('\n');
-    printf("\t\tPlease choose what you want:\n")
+    printf("\t   Please choose what you want:\n");
     printf("\t\t1. insert\n");
     printf("\t\t2. Calculate the Average Score\n");
     printf("\t\t3. Add a new student\n");
@@ -125,4 +81,5 @@ void printWelcome(void)
     printf("\t\t5. Print The table\n");
     printf("\t\t6. Order them by the Ave score\n");
     printStars(n);
+    printf(">>>");
 }
---
+++ StuLinkedList/stuList.h
@@ -0,0 +1,48 @@
+/*stuListModel.h*/
+struct StuInfo
+{
+    char id[20];
+    char name[20];
+    char sex;    //w = female; m = male;
+};
+
+//Student's score
+struct StuScore
+{
+    int chinese;
+    int math;
+    int English;
+    float average;
+};
+
+//LinkedList
+struct node
+{
+    struct StuInfo info;
+    struct StuScore score;
+    int nodeId;     //To tag each node;
+    struct node * next;    //Point to the next;
+};
+typedef struct node node;
+
+//Create and record the data;
+node * creatAList(node * head);
+
+node * deleteAnode(node * head, int nodeId);
+
+void printAList(node * head);
+
+//insert a node inside some ids(中文来讲就是在某序号下一个插入节点)
+node * insertANode(node * head, int nodeId_before);
+
+//So We need to reId all the nodes
+//startId means whether you want to start with 1 or 0;
+node * reIdList(node * head, int startId);
+
+//calculate the average score
+//return whether is successful
+//the head is not changed
+int calTheAve(node * head);
+
+//Order the list
+node * orderAList(node * head);
